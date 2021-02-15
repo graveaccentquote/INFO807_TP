0acd8bdefa7d153088489ce60b7513b23db016a5
C Ost <yuushi_camillkaz@hotmail.fr>
2021-02-15 14:43:48 +0100

    changed Tile to abstract class and filled class

--- src/Tile.java
+++ src/Tile.java
@@ -1,7 +1,11 @@
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("c7d72462-e678-413d-a32e-0f34e97af2f9")
-public interface Tile {
+public abstract class Tile {
+
+    public String tileName;
+
+    ///Methods
     @objid ("3b476daf-4be0-4346-b183-7bd2155efe97")
     void ApplyOnStop(Player player);
 
