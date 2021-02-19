068af0858c3d522cb0510141cbf43b226b406d40
= <nils.ruet@etu.univ-smb.fr>
2021-02-19 15:28:36 +0100

    Filled PropertyLot class

--- src/main/java/PropertyLot.java
+++ src/main/java/PropertyLot.java
@@ -4,6 +4,7 @@ import java.util.List;
 public class PropertyLot extends BuyableLot {
 
     ///Attributes
+    // Inclusive limits of the maximum buildings there can be on a single property of the lot
     public int maxBuildingCount;
     public int minBuildingCount;
 
@@ -11,19 +12,63 @@ public class PropertyLot extends BuyableLot {
     public List<PropertyTile> children = new ArrayList<PropertyTile> ();
 
     ///Constructors
-    public PropertyLot(){} //Default constructor
+    public PropertyLot(){
+        minBuildingCount = 0;
+        maxBuildingCount = 1;
+    } //Default constructor
 
     ///Methods
     public void onBuildEvent() {
+        if(children.size()>0){
+            int max, min;
+            max = min = children.get(0).getBuildingCount();
+            for(PropertyTile p : children){
+                int count = p.getBuildingCount();
+                max = Math.max(count, max);
+                min = Math.min(count, min);
+            }
+            minBuildingCount = min;
+            if(max == min){
+                maxBuildingCount = max+1;
+            } else {
+                maxBuildingCount = max;
+            }
+        }
     }
 
+    // Checks if a number of existing buildings on a tile
+    // allows the owner to build more buildings on it
     public boolean isValidBuildingCount(int buildingCount) {
-        return false; //TODO
+        return buildingCount<maxBuildingCount;
     }
 
     @Override
     public void onOwnershipChange() {
+        boolean isConstructable = true;
+        if(children.size()>0){
+            Player owner = children.get(0).getOwner();
+            if(owner!=null){
+                int i = 1;
+                while(i<children.size() && isConstructable){
+                    if(children.get(i).getOwner() != owner){
+                        isConstructable = false;
+                    }
+                    i++;
+                }
+            } else {
+                isConstructable = false;
+            }
+        }
 
+        if(isConstructable){
+            for(PropertyTile t : children){
+                t.becomeConstructible();
+            }
+        } else {
+            for(PropertyTile t : children){
+                t.becomeUnconstructible();
+            }
+        }
     }
 
     @Override
